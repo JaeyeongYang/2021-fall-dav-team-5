ecd2b1e9cd562c9067438a3de5d09a366a81739e
DurianFarmer <kyuwookchai@gmail.com>
2021-11-12 18:23:22 +0900

    frontend1112

--- frontend/src/Menu.tsx
+++ frontend/src/Menu.tsx
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-=======
 
 
-
->>>>>>> 0c79d81e7b319cacbe347edcc02aa6c0185073d7
 export function Menu(menu: MenuType){
     return <h4>{menu.name}</h4>;
     // 일단 이름만 
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0c79d81e7b319cacbe347edcc02aa6c0185073d7
 export interface MenuType {
 	menuId: number;
 	name: string;
@@ -28,9 +20,5 @@ export interface MenuType {
     url_large: string;
     ingredients: string; 
     recipes: Array<{order: number, text: Text, img: string}>;
-<<<<<<< HEAD
-}
-=======
 }
 
->>>>>>> 0c79d81e7b319cacbe347edcc02aa6c0185073d7

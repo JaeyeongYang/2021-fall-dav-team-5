ef88e7c1cb7dbd0b57c462dd05e2e35b2b0c01bf
JaeyeongYang <jaeyeong.yang1125@gmail.com>
2021-11-28 23:39:48 +0900

    Update the logic for selecting menus

--- frontend/src/components/Visualization.tsx
+++ frontend/src/components/Visualization.tsx
@@ -6,47 +6,41 @@ import PopupWindow from "./PopupWindow";
 import ColorSelector from "./ColorSelector";
 import BubbleChart from "./BubbleChart";
 import { Menu } from "../store/reducers/data";
-import './BubbleChart.css'
+import "./BubbleChart.css";
 import { useAppSelector } from "src/hooks";
-import {
-  selectMenus
-} from "src/store/reducers/data";
+import { selectMenus } from "src/store/reducers/data";
 
 const Visualization = function () {
-    const menus1 = useAppSelector(selectMenus)!;
-    const menus2 = menus1?.slice(1, 600).concat(menus1?.slice(601, 1318));
-    const shuffled = menus2?.sort(() => Math .random() - 0.5 );
-    // const selectedMenus = shuffled?.slice(1, 200);
-    const selectedMenus = shuffled?.slice(1, 20);
+  const menus = useAppSelector(selectMenus) ?? [];
+  // const shuffled = menus?.sort(() => Math.random());
+  const selectedMenus = menus?.slice(0, menus.length > 50 ? 50 : menus.length);
 
-    const selectedKeyHandler = (key: string) => {
-        // eslint-disable-next-line no-alert
-        alert(key)
-      };
-    
-    if (typeof selectedMenus === typeof undefined){
-        console.log('if selected', selectedMenus);
-        return (
-            <div></div>
-        )        
-    } else{
-        console.log('else selected', selectedMenus)
-        return (                
-            <div className='vis-div'>                
-                <div> 
-                    {/* <PopupWindow text="Pie Chart"></PopupWindow> */}
-                    <BubbleChart bubblesData={selectedMenus} width={1300} height={700} textFillColor="drakgrey" backgroundColor="#ffffff" minValue={1} maxValue={300} selectedCircle={selectedKeyHandler} />
-                </div>
-                
-                <div className='scatter-plot-menu-div'>                
-                    <ScatterPlotMenu></ScatterPlotMenu>
-                </div>
-            </div>        
-        )
+  const selectedKeyHandler = (key: string) => {
+    // eslint-disable-next-line no-alert
+    alert(key);
+  };
 
-    }
+  return (
+    <div className="vis-div">
+      <div>
+        {/* <PopupWindow text="Pie Chart"></PopupWindow> */}
+        <BubbleChart
+          bubblesData={selectedMenus}
+          width={1300}
+          height={700}
+          textFillColor="drakgrey"
+          backgroundColor="#ffffff"
+          minValue={1}
+          maxValue={300}
+          selectedCircle={selectedKeyHandler}
+        />
+      </div>
 
+      <div className="scatter-plot-menu-div">
+        <ScatterPlotMenu></ScatterPlotMenu>
+      </div>
+    </div>
+  );
 };
 
 export default Visualization;
-

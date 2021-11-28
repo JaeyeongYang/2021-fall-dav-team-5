ef5d741c29372ee84ec54be150bbe6965985ebcb
JaeyeongYang <jaeyeong.yang1125@gmail.com>
2021-11-28 12:04:56 +0900

    Add stores for filters and UI

--- frontend/src/store/reducers/rootReducer.ts
+++ frontend/src/store/reducers/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import UI from './UI';
 import data from './data';
+import filter from './filter';
 
 export default combineReducers({
   UI,
   data,
+  filter,
 });

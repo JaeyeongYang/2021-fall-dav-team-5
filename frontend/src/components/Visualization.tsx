f0744fdf16fed828ab3c5c402cf86523e3a5b708
JaeyeongYang <jaeyeong.yang1125@gmail.com>
2021-12-01 06:23:58 +0900

    Use pagination with sampled menus to show

--- frontend/src/components/Visualization.tsx
+++ frontend/src/components/Visualization.tsx
@@ -1,41 +1,48 @@
-import "./Visualization.css";
-import ScatterPlotMenu from "./ScatterPlotMenu";
+import { sort } from "d3";
+import React, { useEffect, useState } from "react";
 
-import PopupWindow from "./PopupWindow";
-
-import ColorSelector from "./ColorSelector";
-import BubbleChart from "./BubbleChart";
-import { Menu } from "../store/reducers/data";
-import "./BubbleChart.css";
+import { PAGINATION } from "src/globals";
 import { useAppSelector } from "src/hooks";
-import { selectMenus } from "src/store/reducers/data";
+import { Menu, selectMenus } from "src/store/reducers/data";
+import ScatterPlot from "src/components/charts/ScatterPlot";
+import ScatterPlotMenu from "./ScatterPlotMenu";
+import "./Visualization.css";
 
 const Visualization = function () {
-  const menus = useAppSelector(selectMenus) ?? [];
-  // const shuffled = menus?.sort(() => Math.random());
-  const selectedMenus = menus?.slice(0, menus.length > 50 ? 50 : menus.length);
+  const menus: Menu[] = useAppSelector(selectMenus) ?? [];
+  const [menusShuffled, setMenusShuffled] = useState<Menu[]>([]);
+  const [menusSelected, setMenusSelected] = useState<Menu[]>([]);
+  const [index, setIndex] = useState<number>(0);
 
-  const selectedKeyHandler = (key: string) => {
-    // eslint-disable-next-line no-alert
-    alert(key);
-  };
+  useEffect(() => {
+    if (menus) {
+      const _menu: Menu[] = [...menus];
+      const _index = 0;
+      _menu.sort(() => Math.random() - 0.5);
+
+      setMenusShuffled(_menu);
+      setIndex(_index);
+    }
+  }, [menus]);
+
+  useEffect(() => {
+    if (menusShuffled) {
+      if (index < 0 || index * PAGINATION > menusShuffled.length) {
+        setIndex(0);
+      } else {
+        setMenusSelected(
+          menusShuffled.slice(
+            index * PAGINATION,
+            Math.min((index + 1) * PAGINATION, menusShuffled.length)
+          )
+        );
+      }
+    }
+  }, [menusShuffled, index]);
 
   return (
     <div className="vis-div">
-      <div>
-        {/* <PopupWindow text="Pie Chart"></PopupWindow> */}
-        <BubbleChart
-          bubblesData={selectedMenus}
-          width={1300}
-          height={700}
-          textFillColor="drakgrey"
-          backgroundColor="#ffffff"
-          minValue={1}
-          maxValue={300}
-          selectedCircle={selectedKeyHandler}
-        />
-      </div>
-
+      <ScatterPlot data={menusSelected} width={800} height={600} />
       <div className="scatter-plot-menu-div">
         <ScatterPlotMenu></ScatterPlotMenu>
       </div>
--- frontend/src/globals.ts
+++ frontend/src/globals.ts
@@ -1,1 +1,3 @@
 export const BACKEND_DOMAIN = 'http://127.0.0.1:8000';
+
+export const PAGINATION = 100;

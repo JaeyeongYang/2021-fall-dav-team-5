65d179c68b53b008deb98238315967e7a6cd4fa7
JaeyeongYang <jaeyeong.yang1125@gmail.com>
2021-12-01 06:27:03 +0900

    Remove ExampleComponent from Body

--- frontend/src/components/Body.tsx
+++ frontend/src/components/Body.tsx
@@ -1,15 +1,12 @@
 import "./Body.css";
-import { Tag, BubbleColors } from "src/store/reducers/data";
 
 import SearchBar from "src/components/searchbar/SearchBar";
-import ExampleComponent from "./ExampleComponent";
 import Visualization from "./Visualization";
 
 const Body = function () {
   return (
     <div className="main">
       <SearchBar />
-      <ExampleComponent />
       <Visualization></Visualization>
     </div>
   );
